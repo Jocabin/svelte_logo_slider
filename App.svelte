8a762f2bbe922ef6d2534d594f81e2e74147bfdc
Jocabin <kobyfar@hotmail.com>
2022-09-04 15:28:22 +0200

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Slider from "./lib/Slider.svelte";
+</script>
+
+<main>
+    <div class="wrapper">
+        <Slider count={9} folder="slider"/>
+    </div>
+</main>
+
+<style>
+    main {
+        width: 100%;
+    }
+
+    .wrapper {
+        max-width: 100%;
+        height: 5rem;
+        padding: 2rem;
+    }
+</style>
---
+++ README.md
@@ -0,0 +1,8 @@
+# Svelte slider
+"Infinite logos slider" component made from scratch wit Svelte and pure CSS
+
+# Behavior
+If the logos are too numerous to be all visible on a single line, then the slider is activated.
+
+# Authors
+Par Jocabin
